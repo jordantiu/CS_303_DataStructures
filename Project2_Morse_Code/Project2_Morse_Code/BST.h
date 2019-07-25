267a5c874c0f793fbcf63ea6e09caa3e8ed8882e
jordantiu <47049525+jayceetee23@users.noreply.github.com>
2019-07-24 19:09:03 -0500

    fixed bug with BST.cpp file (removed)

--- Project2_Morse_Code/Project2_Morse_Code/BST.h
+++ Project2_Morse_Code/Project2_Morse_Code/BST.h
@@ -1,30 +1,48 @@
-#include "stdafx.h"
-
+#ifndef BST_H
+#define BST_H
 
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 class BST
 {
 private:
+
 	struct node
 	{
+		//data for node
 		char key;
 		string code;
+
+		//Node pointers
 		node* left;
 		node* right;
 	};
 
+	//root pointer (reference root of tree)
 	node* root;
 
 	void AddLeafPrivate(char key, string code, node* Ptr);
 
-
+	char dot = '.';
+	char dash = '-';
 
 public:
-	BST();
-	node* CreateLeaf(pair<char, string>data);
+
+	BST(); //constructor
+
+	//build populate nodes with data from created map
+	void tree_build(map<char, string>map_var);
+
+
+	node* CreateLeaf(char key, string code);
+
 	void AddLeaf(char key, string code);
+
+
 };
+
+#endif
